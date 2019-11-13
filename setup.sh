453e9e8e68159242477831c7e3b16db759bc3302
Zorro <atossa@protonmail.com>
2019-11-13 20:23:51 +1000

    added colorize gem

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#sudo apt-get install nmap -y
+echo 'Installing Gem: Colorize'
+gem install colorize
+echo 'colorize Installed'
+echo 'Installing Gem: Terminal-Table'
+sudo gem install terminal-table
+echo 'Terminal-Table Installed'
